19d213d4d18172360ad7bc584d710f4c188269ae
Guillaume <guillaume.staub@gmail.com>
2019-10-22 15:55:04 +0200

    Add long labels for currencies and edit the README image

--- app/app.py
+++ app/app.py
@@ -9,7 +9,7 @@ class App(QtWidgets.QWidget) :
         self.c = currency_converter.CurrencyConverter()
         self.setWindowTitle("PyConverter")
         self.setup_ui()
-        self.set_default_values("./currencies.json")
+        self.set_default_values()
         self.setup_connections()
        
 
@@ -20,12 +20,14 @@ class App(QtWidgets.QWidget) :
         self.cbb_devisesTo = QtWidgets.QComboBox()
         self.spn_montantConverti = QtWidgets.QDoubleSpinBox()
         self.btn_inverser = QtWidgets.QPushButton("Reverse currencies")
-        self.currency_From = QtWidgets.QComboBox()
+        self.cbb_currency_long_From = QtWidgets.QComboBox()
+        self.cbb_currency_long_To = QtWidgets.QComboBox()
 
         self.layout.addWidget(self.cbb_devisesFrom)
-        self.layout.addWidget(self.currency_From)
+        self.layout.addWidget(self.cbb_currency_long_From)
         self.layout.addWidget(self.spn_montant)
         self.layout.addWidget(self.cbb_devisesTo)
+        self.layout.addWidget(self.cbb_currency_long_To)
         self.layout.addWidget(self.spn_montantConverti)
         self.layout.addWidget(self.btn_inverser)
         
@@ -35,35 +37,39 @@ class App(QtWidgets.QWidget) :
           return self.contenu
           
 
-    def set_default_values(self,file):
+    def set_default_values(self):
         self.cbb_devisesFrom.addItems(sorted(list(self.c.currencies)))
         self.cbb_devisesTo.addItems(sorted(list(self.c.currencies)))
-        self.currency_From.addItems(sorted(list(self.setup_currency_list(file).values())))
+        self.cbb_currency_long_From.addItems(sorted(list(self.setup_currency_list("./datas/currencies.json").values())))
+        self.cbb_currency_long_To.addItems(sorted(list(self.setup_currency_list("./datas/currencies.json").values())))
         self.cbb_devisesFrom.setCurrentText("EUR")
         self.cbb_devisesTo.setCurrentText("USD")
-        self.currency_From.setCurrentText("Euro")
+        self.cbb_currency_long_From.setCurrentText("Euro")
+        self.cbb_currency_long_To.setCurrentText("Dollar")
         self.spn_montant.setRange(1, 1000000)
         self.spn_montantConverti.setRange(1,1000000)
         self.spn_montant.setValue(100)
         self.spn_montantConverti.setValue(100)
         
-        
 
     def setup_connections(self):
         self.cbb_devisesFrom.activated.connect(self.compute)
         self.cbb_devisesTo.activated.connect(self.compute)
-        self.currency_From.activated.connect(self.compute_2)
+        self.cbb_currency_long_From.activated.connect(self.compute_2)
+        self.cbb_currency_long_To.activated.connect(self.compute_2)
         self.spn_montant.valueChanged.connect(self.compute)
         self.btn_inverser.clicked.connect(self.inverser_devise)
 
     def compute_2(self):
         montant = self.spn_montant.value()
-        devise_name = self.currency_From.currentText()
-        devise_abrg = self.setup_currency_list("./currencies_2.json").get(devise_name)
-        self.cbb_devisesFrom.setCurrentText(devise_abrg)
-        devise_to = self.cbb_devisesTo.currentText()
+        devise_From_name = self.cbb_currency_long_From.currentText()
+        devise_From_abrg = self.setup_currency_list("./datas/currencies_2.json").get(devise_From_name)
+        devise_To_name = self.cbb_currency_long_To.currentText()
+        devise_To_abrg = self.setup_currency_list("./datas/currencies_2.json").get(devise_To_name)
+        self.cbb_devisesFrom.setCurrentText(devise_From_abrg)
+        self.cbb_devisesTo.setCurrentText(devise_To_abrg)
         try : 
-            resultat = self.c.convert(montant, devise_abrg, devise_to)
+            resultat = self.c.convert(montant, devise_From_abrg, devise_To_abrg)
         except currency_converter.currency_converter.RateNotFoundError : 
             print("The conversion didn't work")
         else : 
@@ -72,9 +78,11 @@ class App(QtWidgets.QWidget) :
     def compute(self):
         montant = self.spn_montant.value()
         devise_from = self.cbb_devisesFrom.currentText()
-        devise_name = self.setup_currency_list("./currencies.json").get(devise_from)
-        self.currency_From.setCurrentText(devise_name)
+        devise_From_name = self.setup_currency_list("./datas/currencies.json").get(devise_from)
+        self.cbb_currency_long_From.setCurrentText(devise_From_name)
         devise_to = self.cbb_devisesTo.currentText()
+        devise_To_name = self.setup_currency_list("./datas/currencies.json").get(devise_to)
+        self.cbb_currency_long_To.setCurrentText(devise_To_name)
         try : 
             resultat = self.c.convert(montant, devise_from, devise_to)
         except currency_converter.currency_converter.RateNotFoundError : 
